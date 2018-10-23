bfb96ba19bc63584143c29be910cf38100c3a8ad
goohooh <goo_oo@naver.com>
2018-10-24 07:07:48 +0900

    initial commit

---
+++ bundler.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+function createAsset(filename) {
+    const content = fs.readFileSync(filename, 'utf-8');
+
+    console.log(content);
+}
+
+createAsset('./example/entry.js');
