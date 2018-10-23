4157e05008fc6399884f2f7998a598c9c4a910d8
goohooh <goo_oo@naver.com>
2018-10-24 07:19:52 +0900

    check dependencies

--- bundler.js
+++ bundler.js
@@ -1,9 +1,22 @@
 const fs = require('fs');
+const babylon = require('babylon');
+const traverse = require('babel-traverse').default;
 
 function createAsset(filename) {
     const content = fs.readFileSync(filename, 'utf-8');
+    const ast = babylon.parse(content, {
+        sourceType: "module"
+    });
 
-    console.log(content);
+    const dependencies = [];
+
+    traverse(ast, {
+        ImportDeclaration: ({node}) => {
+            dependencies.push(node.source.value)
+        }
+    });
+
+    console.log(dependencies);
 }
 
 createAsset('./example/entry.js');
