fac99962a593bac46fc138fa6d83cb605dd56bc4
goohooh <goo_oo@naver.com>
2018-10-24 07:29:53 +0900

    collect dependencies

--- bundler.js
+++ bundler.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
+const path = require('path');
 const babylon = require('babylon');
 const traverse = require('babel-traverse').default;
 
+let ID = 0;
 function createAsset(filename) {
     const content = fs.readFileSync(filename, 'utf-8');
     const ast = babylon.parse(content, {
@@ -16,7 +18,39 @@ function createAsset(filename) {
         }
     });
 
-    console.log(dependencies);
+    const id = ID++;
+
+    return {
+        id,
+        filename,
+        dependencies,
+    }
 }
 
-createAsset('./example/entry.js');
+function createGraph(entry) {
+    const mainAsset = createAsset(entry);
+
+    const queue = [mainAsset];
+
+    for(const asset of queue) {
+        const dirname = path.dirname(asset.filename);
+
+        asset.mapping = {};
+
+        asset.dependencies.forEach(relativePath => {
+            const absolutePath = path.join(dirname, relativePath);
+
+            const child = createAsset(absolutePath);
+
+            asset.mapping[relativePath] = child.id;
+
+            queue.push(child);
+        });
+    }
+
+    return queue;
+}
+
+const graph = createGraph('./example/entry.js');
+
+console.log(graph);
